9b6f341ea2fe6b1ca7a24cf4347281325ecc545a
Ana Laura Chenoweth Galaz <analaura@Anas-MacBook-Air.local>
2025-07-17 14:08:57 -0400

    Creating class Lista: Constructor

---
+++ Lista/ListasSimplementeEnlazadas.hpp
@@ -0,0 +1,24 @@
+#ifndef LISTASSIMPLEMENTEENLAZADAS_HPP_INCLUDED
+#define LISTASSIMPLEMENTEENLAZADAS_HPP_INCLUDED
+
+template <typename T>
+
+class ListaSimple
+{
+public:
+    ListaSimple();
+
+private:
+    int tam, pos;
+    struct Elemento
+    {
+        T valor;
+        Elemento *siguiente;
+        Elemento(T val, Elemento *sig) : valor(val), siguiente(sig) {}
+    };
+    Elemento *primero, *ultimo;
+};
+
+#include "ListasSimplementeEnlazadas.tpp"
+
+#endif // LISTASSIMPLEMENTEENLAZADAS_HPP_INCLUDED
