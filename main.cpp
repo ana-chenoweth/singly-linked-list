e5fdfdf8e33d9c1ade12e07efd4952672982b063
Ana Laura Chenoweth Galaz <analaura@Anas-MacBook-Air.local>
2025-07-17 15:04:38 -0400

    Testing another function on main

--- main.cpp
+++ main.cpp
@@ -27,5 +27,25 @@ int main()
     cout << "Lista despues de modificar el elemento en la posicion 2: ";
     lista.Imprimir();
     cout << endl;
+    // Buscar un valor
+    int valor_a_buscar;
+    cout << "\nIngrese el valor que desea buscar en la lista: ";
+    cin >> valor_a_buscar;
+    bool encontrado = lista.BuscarValor(valor_a_buscar);
+    if (encontrado)
+        cout << "El valor " << valor_a_buscar << " se encuentra en la lista." << endl;
+    else
+        cout << "El valor " << valor_a_buscar << " no se encuentra en la lista." << endl;
+
+    // Obtener el primer y ultimo elemento
+    cout << "\nEl primer elemento de la lista es: " << lista.ObtenerPrimero() << endl;
+    cout << "El ultimo elemento de la lista es: " << lista.ObtenerUltimo() << endl;
+
+    // Obtener el numero de elementos en la lista
+    cout << "\nEl numero de elementos en la lista es: " << lista.NumElementos() << endl;
+
+    // Vaciar la lista
+    lista.Vaciar();
+    cout << "\nLa lista ha sido vaciada." << endl;
     return 0;
 }
