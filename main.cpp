a77f2fa46f1aec366f5d26d599fdbb232a037d4d
Ana Laura Chenoweth Galaz <analaura@Anas-MacBook-Air.local>
2025-07-17 15:04:08 -0400

    Testing function in main

--- main.cpp
+++ main.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
+#include "Lista/ListasSimplementeEnlazadas.hpp"
 using namespace std;
 
 int main()
 {
-    
+    ListaSimple<int> lista;
+
+    // Agregar elementos
+    lista.AgregarInicio(1);
+    lista.AgregarInicio(2);
+    lista.AgregarFinal(3);
+    lista.AgregarPos(4, 1); // Agregar en la posicion 1
+
+    // Imprimir la lista
+    cout << "Lista despues de agregar elementos: ";
+    lista.Imprimir();
+    cout << endl;
+
+    // Modificar elemento en la posicion 2
+    cout << "\nModificacion del elemento en la posicion 2:" << endl;
+    cout << "Elemento en la posicion 2 antes de modificar: " << lista.ObtenerPorPos(1) << endl;
+    cout << "Nuevo valor para el elemento en la posicion 2: ";
+    int nuevo_valor;
+    cin >> nuevo_valor;
+    lista.ModificarPos(nuevo_valor, 1); // Utilizamos ModificarPos en lugar de ModificarPorPos
+    cout << "Lista despues de modificar el elemento en la posicion 2: ";
+    lista.Imprimir();
+    cout << endl;
     return 0;
 }
